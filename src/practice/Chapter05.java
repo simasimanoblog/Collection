32667992042ebd447fb9e6ea1fcc92bcb853e12d
SatoshiShimaya <175710380+simasimanoblog@users.noreply.github.com>
2024-08-03 06:15:44 +0900

    課題提出

--- src/practice/Chapter05.java
+++ src/practice/Chapter05.java
@@ -1,29 +1,44 @@
 package practice;
 
-import java.text.ParseException;
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.TreeMap;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Chapter05 {
 
 	public static void main(String[] args) {
+		
 		// インスタンス生成 
-		Task clsTask = new Task();
-		// タスク初期化
-		Map<LocalDate, String> task = new TreeMap<LocalDate, String>();
-		try {
-            // タスク取得
-			clsTask.main(task);
-            // タスクをソートして表示
-			for (Map.Entry<LocalDate, String> taskitem : task.entrySet()) {
-				System.out.println(clsTask.f.format(taskitem.getKey()) + " " + taskitem.getValue());
-			}
-			
-		} catch (ParseException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
+		List<Task> tasks = new ArrayList<>();
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy年M月d日");
+		
+		// ローカル変数定義
+        LocalDate d;
+        // タスクに設定する内容を配列で定義
+		String[] taskday = {"2021年10月21日", "2021年9月15日", "2021年12月4日","2021年8月10日", "2021年11月9日"};
+		String[] taskinfo = {"牛乳を買う。", "○○社面談。", "手帳を買う。","散髪に行く。", "スクールの課題を解く。"};
+		for (int i = 0; i < taskday.length; i++) {
+			// LocalDate に変換
+			d = LocalDate.parse(taskday[i], f);
+			// タスクをリストに追加する
+			tasks.add(new Task(d, taskinfo[i]));
 		}
+        
+        // 日付順に並び替え
+        Collections.sort(tasks, new Comparator<Task>() {
+            @Override
+            public int compare(Task t1, Task t2) {
+                return t1.getDate().compareTo(t2.getDate());
+            }
+        });
+
+        // 並び替え後のタスクを表示
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
 		
 	}
 
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,25 +1,31 @@
 package practice;
 
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Map;
 
 public class Task {
+	private LocalDate date;
+	private String task;
+	DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy年M月d日");
+	
+	// コンストラクタ
+    public Task(LocalDate date, String description) {
+        this.date = date;
+        this.task = description;
+    }
+    
+    public LocalDate getDate() {
+        return date;
+    }
 
-	public DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy年M月d日");
-	public void main(Map<LocalDate, String> task) throws ParseException {
+    public String getTask() {
+        return task;
+    }
 
-		// 文字列からDateインスタンスを生成
-		LocalDate d;
-		
-		String[] taskday = {"2021年10月21日", "2021年9月15日", "2021年12月4日","2021年8月10日", "2021年11月9日"};
-		String[] taskinfo = {"牛乳を買う。", "○○社面談。", "手帳を買う。","散髪に行く。", "スクールの課題を解く。"};
-		for (int i = 0; i < taskday.length; i++) {
-			d = LocalDate.parse(taskday[i], f);
-			task.put(d, taskinfo[i]);
-		}
-
-	}
-
+    @Override
+    public String toString() {
+    	// 出力時に日付形式を元に戻す
+        return f.format(date) + ":" + task;
+    }
+     
 }
